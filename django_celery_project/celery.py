97014c5fcfbf18d73a4200a058a8fa0441475f09
Roopesh <roopesh.rai@plutustec.com>
2022-03-31 19:59:29 +0530

    emailscheduler

--- django_celery_project/celery.py
+++ django_celery_project/celery.py
@@ -1,31 +1,8 @@
-# from __future__ import absolute_import, unicode_literals
-# import os
-# # from .celery import app as celery_app
-# from celery import Celery
-# from django.conf import settings
-#
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_celery_project.settings')
-#
-# app = Celery('django_celery_project')
-# app.conf.enable_utc = False
-#
-# app.conf.update(timezone='Asia/Kolkata')
-#
-# # app.config_from_object(settings, namespace='CELERY')
-# app.config_from_object(settings)
-#
-# # Celery Beat Settings
-#
-# app.autodiscover_tasks(settings.INSTALLED_APPS)
-#
-# @app.task(bind=True)
-# def debug_task(self):
-#     print(f'Request: {self.request!r}')
-
-
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from celery.schedules import crontab
+# from django_celery_beats.models import PeriodicTask
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_celery_project.settings')
 
@@ -35,7 +12,11 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Celery Beat Settings
 app.conf.beat_schedule = {
-
+    'send-mail-every-day-at-12': {
+        'task': 'send_mail_app.tasks.send_mail_func',
+        'schedule': crontab(hour=11, minute=55),
+        # 'args': (2,)
+    }
 }
 
 app.autodiscover_tasks()
--- django_celery_project/settings.py
+++ django_celery_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'mainapp',
     'django_celery_results',
     'django_celery_beat',
+    'send_mail_app',
 ]
 
 MIDDLEWARE = [
@@ -144,7 +145,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'roopesh.rai@plutustec.com'
-EMAIL_HOST_PASSWORD = 'ayfofyccpcjgocoq'
+EMAIL_HOST_PASSWORD = 'Passwordroopesh'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 DEFAULT_FROM_EMAIL = 'Celery <roopesh.rai@plutustec.com>'
